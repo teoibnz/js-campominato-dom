b4f2120392ff2ef62064dfac5a46cd360684da19
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-07 17:17:02 +0100

    aggiunto un ciclo for per generare le 16 mine

--- main.js
+++ main.js
@@ -16,7 +16,12 @@ const playButton = document.querySelector('#play-button');
 
 let  gridWrapper = document.getElementById('grid-wrapper');
 
-let number = 0;
+const bombs = [];
+console.log(bombs)
+
+for ( let i = 1; i <= 16; i++){
+        bombs.push(generateUniqueRandomNumber( bombs, 1,101));
+    } 
 
 playButton.addEventListener('click', function(){
     if (selectItem() == 'facile'){
@@ -24,9 +29,16 @@ playButton.addEventListener('click', function(){
             const addedBox = createNewSquare();
             gridWrapper.appendChild(addedBox);
             addedBox.innerHTML = +i;
+
             addedBox.addEventListener('click', function (){
-                addedBox.classList.add('clicked', 'grid-easy-square');
-                addedBox.classList.add('text-white');
+                if (!bombs.includes(i)){
+                    addedBox.classList.add('clicked');
+                    addedBox.classList.add('text-white');
+                } else {
+                    addedBox.classList.add('bombs')
+                    
+                }
+                
 
             })
         }
@@ -36,7 +48,7 @@ playButton.addEventListener('click', function(){
             gridWrapper.appendChild(addedBox);
             addedBox.innerHTML = +i;
             addedBox.addEventListener('click', function (){
-                addedBox.classList.add('clicked', 'grid-medium-square');
+                addedBox.classList.add('clicked');
                 addedBox.classList.add('text-white');
 
             })
@@ -47,10 +59,52 @@ playButton.addEventListener('click', function(){
             gridWrapper.appendChild(addedBox);
             addedBox.innerHTML = +i;
             addedBox.addEventListener('click', function (){
-                addedBox.classList.add('clicked', 'grid-hard-square');
+                addedBox.classList.add('clicked');
                 addedBox.classList.add('text-white');
             })
         }
     }
 })
+
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. I numeri nella lista delle bombe non possono essere duplicati.
+
+
+
+function generateUniqueRandomNumber( blackList, min, max){
+    // mi creo una variabile inizializzata a false, che mi controlla se ho generato un numero
+    // valido oppure no
+    let check = false;
+    let randomInt;
+
+    // creo un ciclo che continua finché non ho trovato un numero valido (assente in blacklist)
+    while ( !check ){
+        //  genero randomicamente un numero intero tra il min e il max passati come argomenti
+        randomInt  = randomInteger(min, max);
+        // se il numero non è presente nella blacklist allora
+        if ( !blackList.includes(randomInt)  ){
+            // informo il resto della funzione che il numero è stato trovato ed è valido
+            // ==> esco dal ciclo while
+            check = true;
+        }
+    }
+
+    // restituisco il numero valido che ho trovato
+    return randomInt;
+}
+
+
+
+function randomInteger(minimumValue, maximumValue){
+    if ( isNaN(parseInt(minimumValue)) || isNaN(parseInt(maximumValue)) ){
+        console.error('randomInteger(min, max) needs two numbers as argument');
+    }
+
+    return ( Math.floor(Math.random() * ((maximumValue + 1) - minimumValue) + minimumValue));
+}
+
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati
+// - abbiamo calpestato una bomba
+// - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
 
